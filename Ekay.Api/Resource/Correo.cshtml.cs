3332b7cfba48812a1cf351cc5cc8f40c8236d024
19090482@alumno.utmetropolitana.edu.mx <19090482@alumno.utmetropolitana.edu.mx>
2021-03-25 07:46:43 -0600

    Final

---
+++ Ekay.Api/Resource/Correo.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ekay.Api.Source
+{
+    public class CorreoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
